5f8bcbae4f034b5eb63ce7ff26f5a0d06c6b89b7
DarioRamosTec <dario.ramos.tec@gmail.com>
2023-11-21 00:31:24 -0600

    Se agregaron 3 actividades de sesión. Además, se establecio los temas, colores y cadenas de texto. Las cadenas de texto se adaptan al idioma. Por ultimo, existe un ícono preeliminar para la aplicación.

--- app/src/main/java/com/example/pawsecure/RegisterActivity.java
+++ app/src/main/java/com/example/pawsecure/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.pawsecure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.buttonLoginRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
+        });
+    }
+
+    void goToLogin() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/pawsecure/StartActivity.java
+++ app/src/main/java/com/example/pawsecure/StartActivity.java
@@ -2,7 +2,9 @@ package com.example.pawsecure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        findViewById(R.id.buttonLoginStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startIntent(LoginActivity.class);
+            }
+        });
+        findViewById(R.id.buttonSignUpStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startIntent(RegisterActivity.class);
+            }
+        });
+    }
+
+    void startIntent(Class<?> cls) {
+        startActivity(new Intent(this, cls));
+        finish();
     }
 }
