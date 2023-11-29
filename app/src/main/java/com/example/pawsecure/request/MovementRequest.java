951bd1a9aeac96c523cc9e0f98637f0079f87025
RicardoGGx <133252795+RicargoGGx@users.noreply.github.com>
2023-11-29 07:05:08 -0600

    Prototipo: movement

--- app/src/main/java/com/example/pawsecure/request/MovementRequest.java
+++ app/src/main/java/com/example/pawsecure/request/MovementRequest.java
@@ -4,7 +4,7 @@ import com.example.pawsecure.model.Movement;
 import retrofit2.Call;
 import retrofit2.http.GET;
 public interface MovementRequest {
-    @GET("endpoint") // Reemplaza "endpoint" con el endpoint específico de tu API
-    Call<Movement> getData(); // Reemplaza YourResponseModel con el modelo de datos de tu respuesta
+    @GET("endpoint")
+    Call<Movement> getData();
 
 }
